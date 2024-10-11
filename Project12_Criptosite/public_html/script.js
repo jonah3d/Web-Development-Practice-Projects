368981eeec00ddb36f8b8dbdec0a9dada8fe45ba
jonah3d <84209915+jonah3d@users.noreply.github.com>
2024-10-11 14:02:50 +0200

    Criptosite

---
+++ Project12_Criptosite/public_html/script.js
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+let exlore = document.querySelector('.buto');
+
+exlore.addEventListener('click', function() {
+            alert('POL CO PILOT');
+        });
+
